d3bba1540ec1249334c74d382441aa30077687fc
cadenph3 <65471128+cadenph3@users.noreply.github.com>
2020-05-17 22:58:28 -0400

    Update reaction.cc

--- moderation/VerifyLog/reaction.cc
+++ moderation/VerifyLog/reaction.cc
@@ -1,22 +1,21 @@
 {{/*Config Options*/}}
-
-{{$msgID := 708538517604532516}}
+ 
+{{$msgID :=  708538517604532516}}
 {{$oe := "✅"}}
-{{$log := 703043667321028688}}
 {{$MemberRole := "Members"}}
-
+ 
+{{$log := 703043667321028688}}
+ 
 {{/*Code*/}}
-
+ 
 {{$RID := .ReactionMessage.ID}}
-{{$user := joinStr " " (toString (.User.ID)) "aka" (.User.String)}}
+{{$UID := (toString (.User.ID)) }}
+{{$USN := (toString (.User.String))}}
+{{$msg := "User has verified their account"}}
+{{$avatar := (.User.AvatarURL "32")}}
 {{if (and (.ReactionAdded) (eq $RID $msgID) (eq .Reaction.Emoji.Name $oe))}}
 	{{if not (hasRoleName $MemberRole)}}
-
-{{/*If you wish to edit the log message, do so here*/}}
-  {{$msg := "Member has verified their account"}}
-	{{ $embed := cembed "title" $user "description" $msg "color" 9021952 "timestamp"  currentTime }}
-	
-
-  {{$msgID := sendMessageNoEscapeRetID $log $embed}}
+		{{ $embed := cembed "image" (sdict "url" $avatar)  "fields" (cslice  (sdict "name" "User ID:" "value" $UID "inline" true)  (sdict "name" "Username:" "value" $USN "inline" true) (sdict "name" "Content" "value" $msg "inline" false)) "color" 9021952 "timestamp"  currentTime }}
+		{{sendMessageNoEscape $log $embed}}
 	{{end}}
 {{end}}
